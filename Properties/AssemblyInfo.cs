32068747b4299edd12dd5e2118d421263ea26b64
Herp Derpinstine <herpderpinstine@gmail.com>
2020-12-16 22:38:24 -0600

    Initial Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImageConversion extension for Il2Cpp Games")]
+[assembly: AssemblyDescription("ImageConversion extension for Il2Cpp Games")]
+[assembly: AssemblyCompany(MelonLoader.BuildInfo.Company)]
+[assembly: AssemblyProduct("UnityEngine.Il2CppImageConversionManager")]
+[assembly: AssemblyCopyright("Created by " + MelonLoader.BuildInfo.Author)]
+[assembly: AssemblyTrademark(MelonLoader.BuildInfo.Company)]
+[assembly: Guid("739086B3-925B-4DCA-BEEF-3F988F511FB9")]
+[assembly: AssemblyVersion(MelonLoader.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(MelonLoader.BuildInfo.Version)]
